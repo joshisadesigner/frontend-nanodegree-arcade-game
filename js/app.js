1cb09f8f3a094957044c755f93a12836728fa260
Josh <joshisadesigner@gmail.com>
2017-05-26 18:06:20 -0600

    Add first lines

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+var tileh = 101,
+    tilew = 80;
+    canvasw = tilew * 5;
+    canvash = tileh * 6;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,7 +11,19 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 60;
+    this.speed = Math.floor(Math.random() * 1000);
 };
+
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  // Placing on the center of the canvas
+  // as the canvas width is 505 and each tile o cupy an area of 101 height and
+  // 80px width with a tilew variable
+  this.x = 0 ;
+  this.y = -20 ; // placed on the first grass row
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +31,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += ( this.speed * dt );
+    console.log( this.x );
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +43,46 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
 
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function( key ) {
+  switch( key ){
+    case 'up' :
+      if ( this.y > 20 ){ this.y -= 80; }
+      console.log( 'Boy is at y = ' + this.y + '(up)');
+      break;
+    case 'down' :
+    if ( this.y < ( canvash - tileh * 3 ) ){ this.y += 80 ; }
+      console.log( 'Boy is at y = ' + this.y + '(down)');
+      break;
+    case 'left' :
+      if ( this.x > 20 ){ this.x -= 101; }
+      console.log( 'Boy is at x = ' + this.x + '(right)');
+      break;
+    case 'right' :
+    if ( this.x < ( canvasw - tilew ) ){ this.x += 101 ; }
+      console.log( 'Boy is at x = ' + this.x + '(left)');
+      break;
+  }
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var bug1 = new Enemy();
+
+var allEnemies = [ bug1 ];
+var player = new Player();
 
 
 
